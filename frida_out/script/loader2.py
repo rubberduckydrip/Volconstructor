113761350cc4dbf4ce5ef3b88fdde42c4b2ce006
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-02-21 12:01:59 +0100

    Added more comments

--- frida_out/script/loader2.py
+++ frida_out/script/loader2.py
@@ -6,15 +6,18 @@ import time
 from adbutils import adb
 import os
 
-# Global variables
+## Global variables
 package = "com.example.dynamiccodeloadingexample"
 
-# Functions
+## Functions
+
+# Function to handle messages from the script
 def on_message(message, data):
     if(message['type'] == 'send'):
         print("[+] Received: " + message['payload'])
         download(message['payload'])
 
+# Function to download the file
 def download(item):
     device = adb.device()
 
@@ -26,6 +29,7 @@ def download(item):
 print("[+] Waiting for process to start...")
 device = frida.get_usb_device()
 
+# Wait for the process to start
 while True:
     try:
         process = device.get_process(package)
@@ -35,15 +39,13 @@ while True:
         
         time.sleep(0.01)
 
-# time.sleep(1) #Without it Java.perform silently fails
+# Attach to the process
 session = device.attach(process.pid)
 script = session.create_script(open("script.js").read())
 script.on('message', on_message)
 script.load()
 package_path = "/data/user/0/" + package + "/dumps/"
 script.post({"type": "path", "payload": package_path})
-# script.post({"type": "directory", "value": package})
-# print("[+] Sent directory to script.js")
 
 # prevent the python script from terminating
 input()
--- frida_out/script/script.js
+++ frida_out/script/script.js
@@ -4,17 +4,20 @@ recv('path', function onMessage(message) {
   console.log("[*] Received poke message: " + message.payload);
   const path = message.payload;
 
+  // Waits an amount of time to allow the dex file to be loaded
   setTimeout(function() {
     var counter = 0;
     // Iterate over all loaded modules to find the one that contains the class
     Process.enumerateModules({
       onMatch: function (module) {
+        // Check if the module name contains "dex"
         console.log("[*] Checking module " + module.name);
         if (module.name.includes("dex")) {
           var base = module.base;
           var size = module.size;
           console.log("[*] Found module " + module.name + " at " + base + " with size " + size);
           
+          // Dump the module
           console.log("[*] Dumping " + module.name);
           var ranges = Process.enumerateRangesSync({protection: 'r--', coalesce: true});
           for (var i = 0; i < ranges.length; i++) {
@@ -27,6 +30,7 @@ recv('path', function onMessage(message) {
               fd.flush();
               fd.close();
               console.log("[*] Done!");
+              // Send the path of the dumped module
               send(filename);
               break;
             }
